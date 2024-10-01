9bae889c3d6a010c93edf9d37275bb31818180cd
Altriana <150026999+Altriana@users.noreply.github.com>
2024-10-01 11:10:16 +0200

    Update 02-form.js

--- src/02-form.js
+++ src/02-form.js
@@ -1,26 +1,36 @@
-console.log(localStorage);
 
 const form = document.querySelector(".feedback-form");
-const textarea = form.elements.message;
-const emailInput = form.elements.email;
 const localStorageKey = "feedback-form-state";
+const emailInput = form.elements.email;
+const textarea = form.elements.message;
 
 
 const localStorageValue = () => {
     const object = {
         email: emailInput.value,
         message: textarea.value,
-    }
+    };
     localStorage.setItem(localStorageKey, JSON.stringify(object));
 };
 
-
 form.addEventListener("input", () => {
-    localStorage.setItem(localStorageKey, localStorageValue);
+  localStorageValue();
 });
-form.addEventListener("textarea", () => {
-    localStorage.setItem(localStorageKey, localStorageValue);
-});
-textarea.value = localStorage.getItem(localStorageKey, localStorageValue);
-emailInput.value = localStorage.getItem(localStorageKey, localStorageValue);
+    const objectValue = localStorage.getItem(localStorageKey);
+    const value = JSON.parse(objectValue);
+    textarea.value = value.message;
+    emailInput.value = value.email; 
 
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    if (emailInput.value === "" || textarea.value === "") {
+   return alert("All form fields must be filled in");
+   };
+    console.log({
+        email: emailInput.value.trim(),
+        message: textarea.value.trim(),
+    });
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
